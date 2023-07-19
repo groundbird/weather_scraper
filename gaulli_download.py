d4f6e3f2360bfef5ff33842368fbe28624e6a8dd
6vpika <t.tanaka@astr.tohoku.ac.jp>
2023-07-19 05:25:20 +0000

    Add domeclose error massage

--- gaulli_download.py
+++ gaulli_download.py
@@ -24,13 +24,14 @@ class GaulliDownloadDataType(IntEnum):
     METEO    = 5      # model/sensor
 
 class GaulliDownloader:
-    def __init__(self, url=URL_GAULLI, pwv_type='p', num_days=5):
+    def __init__(self, url=URL_GAULLI, pwv_type='p', num_days=30):
         self._url = url
         self._pwv_type = pwv_type # "r": rapid=express, "p": precise=final
         self._num_days = num_days # days to be downloaded at each time, should be >2 if pwv_type="p"
 
     def get(self):
-        os.system(f"wget --no-check-certificate --post-data 'pwv_type={self._pwv_type}&num_days_download={self._num_days}'  {self._url} -O {FNAME_TMP} > /dev/null 2> /dev/null")
+        os.system(f"wget --no-check-certificate --post-data \'pwv_type={self._pwv_type}&num_days_download={self._num_days}\'  {self._url} -O {FNAME_TMP}")# > /dev/null 2> /dev/null")
+        print(f"wget --no-check-certificate --post-data 'pwv_type={self._pwv_type}&num_days_download={self._num_days}'  {self._url} -O {FNAME_TMP} > /dev/null 2> /dev/null")
         f = open(FNAME_TMP)
         lines = f.readlines()
         vals = [line.strip().strip('\"\'').split(',') for line in lines if line.strip().strip('\"\'')[0]!='#']
@@ -40,18 +41,19 @@ class GaulliDownloader:
             v = [float(x) for x in val[2:-1]] + [val[-1]]
             return (t,v)
         vals = [convert(ival) for ival in vals]
-            
+
         return vals
 
 def main():
     gaulli = GaulliDownloader()
     vals = gaulli.get()
+    print(len(vals))
     dt,data = vals[-1]
 
     print('Update: ', dt, data[-1])
     for val in data[:-1]:
         print(val)
+        print(data)
 
 if __name__ == '__main__':
     main()
-
