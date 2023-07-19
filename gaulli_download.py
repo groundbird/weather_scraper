953a65026aa606af944cf8ea5f90f837e8ff00ba
6vpika <t.tanaka@astr.tohoku.ac.jp>
2023-07-19 08:51:45 +0000

    remove print

--- gaulli_download.py
+++ gaulli_download.py
@@ -30,8 +30,8 @@ class GaulliDownloader:
         self._num_days = num_days # days to be downloaded at each time, should be >2 if pwv_type="p"
 
     def get(self):
-        os.system(f"wget --no-check-certificate --post-data \'pwv_type={self._pwv_type}&num_days_download={self._num_days}\'  {self._url} -O {FNAME_TMP}")# > /dev/null 2> /dev/null")
-        print(f"wget --no-check-certificate --post-data 'pwv_type={self._pwv_type}&num_days_download={self._num_days}'  {self._url} -O {FNAME_TMP} > /dev/null 2> /dev/null")
+        os.system(f"wget --no-check-certificate --post-data 'pwv_type={self._pwv_type}&num_days_download={self._num_days}'  {self._url} -O {FNAME_TMP} > /dev/null 2> /dev/null")
+        # print(f"wget --no-check-certificate --post-data 'pwv_type={self._pwv_type}&num_days_download={self._num_days}'  {self._url} -O {FNAME_TMP} > /dev/null 2> /dev/null")
         f = open(FNAME_TMP)
         lines = f.readlines()
         vals = [line.strip().strip('\"\'').split(',') for line in lines if line.strip().strip('\"\'')[0]!='#']
@@ -47,13 +47,11 @@ class GaulliDownloader:
 def main():
     gaulli = GaulliDownloader()
     vals = gaulli.get()
-    print(len(vals))
+#    print(len(vals))
     dt,data = vals[-1]
 
     print('Update: ', dt, data[-1])
     for val in data[:-1]:
         print(val)
-        print(data)
-
 if __name__ == '__main__':
     main()
